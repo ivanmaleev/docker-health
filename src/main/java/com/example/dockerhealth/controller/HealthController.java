c13ae358b820a2e0cd399d8f570c05d740a10466
Ivan Maleev <ivanmaleev@inbox.ru>
2023-10-08 10:54:22 +0300

    add random 500 status

--- src/main/java/com/example/dockerhealth/controller/HealthController.java
+++ src/main/java/com/example/dockerhealth/controller/HealthController.java
@@ -1,15 +1,27 @@
 package com.example.dockerhealth.controller;
 
 import com.example.dockerhealth.dto.HealthDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+import java.util.Random;
+
 @RestController
 public class HealthController {
 
+    @Value("${health.random.max}")
+    private Integer randomMax;
+
+    private final Random random = new Random();
+
     @GetMapping("/health")
     public ResponseEntity<HealthDto> getHealthStatus() {
+        if (Objects.equals(random.nextInt(randomMax), randomMax - 1)) {
+            return ResponseEntity.internalServerError().build();
+        }
         return ResponseEntity.ok(new HealthDto("OK"));
     }
 }
--- src/main/java/com/example/dockerhealth/controller/UserController.java
+++ src/main/java/com/example/dockerhealth/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.dockerhealth.controller;
 import com.example.dockerhealth.entity.User;
 import com.example.dockerhealth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,25 +13,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
+import java.util.Objects;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
+
+    @Value("${health.random.max}")
+    private Integer randomMax;
+
+    private final Random random = new Random();
 
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping("")
     public ResponseEntity<User> create(@RequestBody User user) {
+        if (Objects.equals(random.nextInt(randomMax), randomMax - 1)) {
+            return ResponseEntity.internalServerError().build();
+        }
         return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<User> get(@PathVariable Long userId) {
+        if (Objects.equals(random.nextInt(randomMax), randomMax - 1)) {
+            return ResponseEntity.internalServerError().build();
+        }
         return new ResponseEntity<>(userRepository.findById(userId).orElseThrow(), HttpStatus.OK);
     }
 
